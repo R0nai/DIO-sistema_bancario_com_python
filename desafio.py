395bcc974fce9bc9419a0d1f8273d4205805be0c
Ronai Melo <48307560+R0nai@users.noreply.github.com>
2023-07-22 16:01:15 -0300

    Update desafio.py with menu

--- desafio.py
+++ desafio.py
@@ -1,1 +1,14 @@
+#menu recriado do repositório trilha-python-dio
 
+menu = """\n
+    ================ MENU ================
+    [d]\tDepositar
+    [s]\tSacar
+    [e]\tExtrato
+    [nc]\tNova conta
+    [lc]\tListar contas
+    [nu]\tNovo usuário
+    [q]\tSair
+    => """
+
+print(menu)
